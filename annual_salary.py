a415982daaeff708f4db80ce3b33dfa925fc00dc
Rodrigo Schammass <roschammass@gmail.com>
2022-01-20 15:52:36 -0500

    Add files via upload

---
+++ annual_salary.py
@@ -0,0 +1,7 @@
+# Convert my hourly wage to an equivalent annual salary
+
+hourly_wage = input("What is your salary per hour?")
+worked_hours_per_week = input("How many hours do you work per week(1-80)?")
+work_weeks_per_year = input("How many weeks do you work per week (1 to 52)?")
+annual_salary = float(hourly_wage) * float (worked_hours_per_week) * float(work_weeks_per_year)
+print("Your annual salary is " + str(annual_salary))
